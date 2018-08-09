9f521a60325613b29662067b61ccd91f1bafd331
Vitaliy S. Orlov <orlov0562@gmail.com>
2018-08-09 13:53:18 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -13,11 +13,11 @@ command -v kdialog >/dev/null 2>&1 || { echo "I require 'kdialog' but it's not i
 
 CONFIRM=""
 
-DOMAIN=`kdialog --title "Domain" --inputbox "Please enter domain:" "domain.sv"` 
+DOMAIN=`kdialog --title "Domain" --inputbox "Please enter domain:" "domain.sv"`
 CONFIRM="${CONFIRM}Domain: $DOMAIN\n"
 
 FRAMEWORK=`kdialog --radiolist "Select framework:" \
-    yii "Yii 2" on \
+    yii2basic "Yii 2 (Basic)" on \
     no "No framework" off \
 `
 if [ $? = 1 ]; then
@@ -37,10 +37,10 @@ fi
 kdialog --title "Database" --yesno "Create database?"
 if [ $? = 0 ]; then
     DEF_DB_NAME=${DOMAIN%.sv}
-    DB_NAME=`kdialog --title "Database name" --inputbox "Please enter database name:" "$DEF_DB_NAME"` 
+    DB_NAME=`kdialog --title "Database name" --inputbox "Please enter database name:" "$DEF_DB_NAME"`
     if [ $? = 1 ]; then
         exit
-    fi    
+    fi
     CONFIRM="${CONFIRM}Database: $DB_NAME\n"
     CREATE_DB="yes"
 else
@@ -54,10 +54,10 @@ if [ $? = 1 ]; then
     exit
 else
     PROGRESS=`kdialog --progressbar "Create project" 6`
-    
+
     qdbus $PROGRESS Set "" value 1
-    qdbus $PROGRESS setLabelText "Create domain directories.."    
-    
+    qdbus $PROGRESS setLabelText "Create domain directories.."
+
     # Create dirs
     mkdir -p "$SITE_DIR/$DOMAIN/public_html"
     mkdir -p "$SITE_DIR/$DOMAIN/logs"
@@ -65,41 +65,48 @@ else
 
     qdbus $PROGRESS Set "" value 1
     qdbus $PROGRESS setLabelText "Create virtual domain configuration file.."
-    
+
     #create conf file
     php "$BASE_DIR/conf-template.php" "$DOMAIN" "$FRAMEWORK" "$MOD_REWRITE" > "$CONF_DIR/$DOMAIN.conf"
 
-    
     #create db
     if [ $CREATE_DB = "yes" ]; then
         qdbus $PROGRESS Set "" value 2
         qdbus $PROGRESS setLabelText "Create database.."
 
         mysql --defaults-extra-file="$BASE_DIR/mysql.cnf" -e"CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-    fi    
-    
+    fi
+
     #append to hosts
-    
+
     qdbus $PROGRESS Set "" value 3
     qdbus $PROGRESS setLabelText "Append record to host file"
-    
-    kdesudo -c"echo '127.0.0.1 $DOMAIN www.$DOMAIN' >> $HOSTS_FILE"
-    
+
+    #kdesudo -c"echo '127.0.0.1 $DOMAIN www.$DOMAIN' >> $HOSTS_FILE"
+    kdesudo $BASE_DIR/hosts.sh $DOMAIN
+
     #install frameworks
-    if [ $FRAMEWORK = "yii" ]; then
+
+    if [ $FRAMEWORK = "yii2basic" ]; then
         qdbus $PROGRESS Set "" value 4
-        qdbus $PROGRESS setLabelText "Create yii project.."
-    
+        qdbus $PROGRESS setLabelText "Create yii2basic project.."
+
         composer create-project --prefer-dist yiisoft/yii2-app-basic "$SITE_DIR/$DOMAIN/public_html/"
+        chmod +x $SITE_DIR/$DOMAIN/public_html/yii
+
+        if [ $CREATE_DB = "yes" ]; then
+            sed -i "s/dbname=yii2basic/dbname=$DB_NAME/g" $SITE_DIR/$DOMAIN/public_html/config/db.php
+            $SITE_DIR/$DOMAIN/public_html/yii migrate --interactive=0
+        fi
     fi
-    
+
     #restart apache
     qdbus $PROGRESS Set "" value 5
     qdbus $PROGRESS setLabelText "Restart Apache.."
-    
-    kdesudo "service apache2 restart"
-    
+
+    kdesudo /bin/systemctl restart apache2.service
+
     qdbus $PROGRESS close
-    
+
     kdialog --msgbox "Project succefully created!"
 fi
